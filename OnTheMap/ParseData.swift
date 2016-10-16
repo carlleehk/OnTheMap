9c9763fec164fe88908ae3f0d101792ae9c88b63
carlleehk <carllee.hk@gmail.com>
2016-10-15 21:18:12 -0400

    getting user location

---
+++ OnTheMap/ParseData.swift
@@ -0,0 +1,13 @@
+//
+//  ParseData.swift
+//  OnTheMap
+//
+//  Created by Carl Lee on 10/15/16.
+//  Copyright © 2016 Carl Lee. All rights reserved.
+//
+
+struct parseInfo{
+    static var locationLong: Double? = nil
+    static var locationLat: Double? = nil
+
+}
